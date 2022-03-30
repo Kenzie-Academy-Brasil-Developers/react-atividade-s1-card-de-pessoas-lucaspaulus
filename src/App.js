2ed373fa67ea43a7d7b4da3972fa5c8034062602
Lucas Paulus <lucas.apaulus012@gmail.com>
2022-03-30 20:58:43 -0300

    usei o map para percorrer o array de objetos

--- src/App.js
+++ src/App.js
@@ -5,33 +5,34 @@ function App() {
     const users = [
       
         {
+        id:1,
         Dev: "Gabriel",
         Idade: 19,
         Pais: "Brasil"
         },
         {
+        id:2, 
         Dev: "Filipe",
         Idade: 28,
         Pais: "Brasil"
         },
         {
+        id:3,
         Dev: "Mariana",
         Idade: 24,
         Pais: "Itália"
         }
     ]
 
+
  
     return (
         <div className="App">
             <header className="App-header">
                 <div className="App-galery">
-                    <Developers name={users[0].Dev} age={users[0].Idade} country={users[0].Pais}/>
-                    <Developers name={users[1].Dev} age={users[1].Idade} country={users[1].Pais}/>
-                    <Developers name={users[2].Dev} age={users[2].Idade} country={users[2].Pais}/>
-                    <Developers name={users[0].Dev} age={users[0].Idade} country={users[0].Pais}/>
-                    <Developers name={users[1].Dev} age={users[1].Idade} country={users[1].Pais}/>
-                    <Developers name={users[2].Dev} age={users[2].Idade} country={users[2].Pais}/>
+                    {users.map((pessoa)=>(
+                          <Developers key={pessoa.id} name={pessoa.Dev} age={pessoa.Idade} country={pessoa.Pais}/>
+                    ))}
                 </div>
                
             </header>
